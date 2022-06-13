2b0180a0d764dd052bbc01d00a0a6dac3ffb8631
Anderson Marin <andersonarturomarin@gmail.com>
2022-06-12 22:23:22 -0500

    version-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,42 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SlidebarComponent } from './components/slidebar/slidebar.component';
+import { CardComponent } from './components/card/card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CalificacionComponent } from './calificacion/calificacion.component';
+import { MantenimientoComponent } from './mantenimiento/mantenimiento.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+// rutas de la aplicacion
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'mantenimiento', component: MantenimientoComponent },
+  { path: 'calificacion', component: CalificacionComponent },
+  { path: 'contacto', component: ContactoComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SlidebarComponent,
+    CardComponent,
+    FooterComponent,
+    HomeComponent,
+    CalificacionComponent,
+    MantenimientoComponent,
+    ContactoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // importantre agregar el routerModule con forRoot le decimos las rutas
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
